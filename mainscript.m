0e02d90e23ece673e939178a6be2382192fe8a92
Lucas Abdalah <39392860+lucasabdalah@users.noreply.github.com>
2019-04-04 18:05:50 -0300

    Script v1.0
    - Redução do script para ficar semelhante à main de uma função
    -

--- mainscript.m
+++ mainscript.m
@@ -1,5 +1,4 @@
-% Projeto de Iniciacao Cientifica - UFC
-% Recebe o vetor de canais x sinal (no tempo)
+% PIBIC - UFC
 
 clear all; close all; clc; % Clear all the workspace
 %Tempo=datetime('now','Format','HH:mm:ss.SSS');
@@ -7,10 +6,8 @@ clear all; close all; clc; % Clear all the workspace
 % Load your working dataet
 % The variable ictal.mat is 50 x 1024 matrix
 
-
 load('ictal'); % Carrega os dados de 'ictal'
 ictal=a; % Recebe a matriz
 
 % Function that return the X tensor (Channel X Signal X Frequency)
 X=eeg2tensor(ictal);
-

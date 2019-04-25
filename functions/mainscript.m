b8b789e11c7859fd1622cfe380e3155eac29b83d
Lucas Abdalah <39392860+lucasabdalah@users.noreply.github.com>
2019-04-25 15:27:34 -0300

    Descrição e Tempo com a Frequência de Amostramgem
    - Alterações na mainscript.m com descrição do que deve constrar da main, além do cálculo do eixo x do tempo em função da frequência de amostragem do sinal;
    
    - Redução da imagem Fluxograma.png.

--- functions/mainscript.m
+++ functions/mainscript.m
@@ -6,8 +6,35 @@ clear all; close all; clc; % Clear all the workspace
 % Load your working dataet
 % The variable ictal.mat is 50 x 1024 matrix
 
+fs=256; % Samples/second
+
 load('ictal'); % Carrega os dados de 'ictal'
-ictal=a; % Recebe a matriz
+
+% Transpose DATA to result in a matrix Signal X Channel(1024x50)
+ictal=a'; % Recebe a matriz
+
+% Centering and Scaling
+	%ictal=center_scale(ictal);
+
+% Skewness
+	%ictal=skewness(ictal);
+
+
+% Time (s) for X axis
+	a1=ictal(:,1);
+	ts=1/fs;
+	n1=numel(a1);
+
+	t=0:ts:(n1-1)*ts;
+
+plot(a1,t);
+
+% PCA 
+
+
+% TENSOR
+
+
 
 % Function that return the X tensor (Channel X Signal X Frequency)
 X=eeg2tensor(ictal);

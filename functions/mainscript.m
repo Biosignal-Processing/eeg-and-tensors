44d298ab2ac17d9fc7750fc8cfdf7256a192d1ca
Lucas Abdalah <39392860+lucasabdalah@users.noreply.github.com>
2019-05-08 13:13:23 -0300

    HeatMap, Corplot, PCA e ScreePlot
    - Adição dos passos do título no código;
    - Plots adicionados a respectiva pasta.

--- functions/mainscript.m
+++ functions/mainscript.m
@@ -1,4 +1,6 @@
 % PIBIC - UFC
+% EEG and Tensors
+
 
 clearvars; close all; clc; % Clear all the workspace
 %Tempo=datetime('now','Format','HH:mm:ss.SSS');
@@ -13,6 +15,7 @@ x=a'; % Recebe a matriz
 
 % Clear any variable different of x 
 clearvars -except x
+clc;
 
 fs=256; % Samples/second
 
@@ -22,19 +25,23 @@ fs=256; % Samples/second
 	ts=1/fs;
 	t=0:ts:(samples-1)*ts;
     clear ts;
-    
- figure
+
+% Original Signal Plot
+figure
     plot(t,x(:,1))
     title('Canal 1: Original')
     xlabel('Tempo (s)');
     ylabel('Amplitude (mV)')    
     grid on   
- 
+
+    
+disp('Press Enter to Center and Scale'); 
+pause
+clc
 % Centering and Scaling
 	x_cs=center_scale(x);
-    
-pause
 
+% Normalized Signal Plot
 figure
     plot(t,x_cs(:,1),'r');
     title('Canal 1: Normalizado')
@@ -42,7 +49,64 @@ figure
     ylabel('Amplitude Normalizada (mV)')    
     grid on
     
-% Function that return the y tensor (Channel X Signal X Frequency)
+    
+disp('Press Enter to Covariance Analysis'); 
+pause
+clc
+% Analise de Covariancia   
+    cov_x=cov(x_cs);
+% Caso Geral da Covariancia
+figure
+    h=heatmap(cov_x);
+    h.Colormap=cool;
+    h.ColorLimits=[-1 1];
+    title('Mapa de Calor')
+    xlabel('Canais')
+    ylabel('Canais')
 
-% 
+% Caso da Covariancia Positiva
+figure
+	h07 = double(cov_x>0.7);
+    h07=heatmap(h07);
+    h07.Colormap=cool;
+    h07.ColorLimits=[-1 1];
+    colorbar off
+    title('Mapa de Calor: Covâriancia > 0.7 ')
+    xlabel('Canais')
+    ylabel('Canais')
+
+% Caso da Covariancia Positiva
+figure
+	hminus07 = double(cov_x<-0.7);
+    hminus07=heatmap(hminus07);
+    hminus07.Colormap=cool;
+    hminus07.ColorLimits=[-1 1];
+    colorbar off
+    title('Mapa de Calor: Covâriancia < -0.7 ')
+    xlabel('Canais')
+    ylabel('Canais')
+
+disp('Press Enter to PCA'); 
+pause
+clc
+% PCA - Dimensionasionality Reduction
+[coeff,score,latent]=pca(x_cs);
+
+% Scree Plot
+    figure
+    scatter(1:channels,latent);
+    hold on
+    plot(1:channels,latent);
+    yyaxis left;
+    title('ScreePlot')
+    xlabel('Componentes')
+    ylabel('Variância')
+    yyaxis right;
+    ylim([0 100]);
+    ylabel('Percentual')
+    grid on
+    
+% ALS
+% Function that return the y tensor (Channel X Signal X Frequency)
+ 
 y=eeg2tensor(x);
